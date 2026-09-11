0e2de4267d21cd90c59dd48d805fde975213a6f4
Tommaso <atlaslanternproject@gmail.com>
2026-09-11 23:46:41 +0200

    hide Android system navigation bar

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,13 +1,25 @@
 ﻿import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'router.dart';
 import '../core/theme/app_theme.dart';
 import '../l10n/app_localizations.dart';
 import '../features/settings/providers/language_provider.dart';
-class AmaterasuTripApp extends ConsumerWidget {
+class AmaterasuTripApp extends ConsumerStatefulWidget {
   const AmaterasuTripApp({super.key});
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  ConsumerState<AmaterasuTripApp> createState() => _AmaterasuTripAppState();
+}
+class _AmaterasuTripAppState extends ConsumerState<AmaterasuTripApp> {
+  @override
+  void initState() {
+    super.initState();
+    SystemChrome.setEnabledSystemUIMode(
+      SystemUiMode.immersiveSticky,
+    );
+  }
+  @override
+  Widget build(BuildContext context) {
     final locale = ref.watch(languageProvider);
     return MaterialApp.router(
       title: 'AmaterasuTrip',
